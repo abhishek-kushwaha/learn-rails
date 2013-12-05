759cdd437beabd9d5437cd6a36bd1d30a13e7926
Abhishek-kushwaha <akushwaha@q3tech.com>
2013-12-05 18:30:51 +0530

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ LearnRails::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
+  
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -26,6 +27,7 @@ LearnRails::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
