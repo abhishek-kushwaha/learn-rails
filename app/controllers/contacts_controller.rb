e22856f5b57237756bd87f475bbe30784e4e0a00
Abhishek-kushwaha <akushwaha@q3tech.com>
2013-12-16 18:16:58 +0530

    Sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,9 @@ class ContactsController < ApplicationController
 		if @contact.valid?
 			@contact.update_spreadsheet
 
-			# TODO send message
-			flash[:notice] = "Message sent from #{@contact.name}."
+			UserMailer.contact_email(@contact).deliver
+
+			flash[:notice] = "Message sent from #{@contact.name} :-)."
 			redirect_to root_path
 		else
 			render :new
